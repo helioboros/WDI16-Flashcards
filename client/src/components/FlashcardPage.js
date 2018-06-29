ad308c37539cdaf1ab3200f34d33a3a0258f5b66
helioboros <felinewraith@gmail.com>
2018-06-29 13:53:33 -0400

    individual flashcards on main flashcard page

--- client/src/components/FlashcardPage.js
+++ client/src/components/FlashcardPage.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react"
 
 class FlashcardPage extends Component {
-    //DISPLAY ALL FLASHCARDS
+    //handleChange
+    //updateFlashcard
     render() {
         return (
             <div>
                 <div>
-                    <h1>{this.state.user.userName}'s flashcard Board</h1>
+                    <h1>{this.state.subject.subjectTitle}</h1>
                     <button onClick={this.createNewFlashcard}>New flashcard</button>
                 </div>
-                <div>
+                {/* <div>
                     {this.state.flashcards.map(flashcard => {
                         return (
-// uuuh flashcards
+                            <div key={flashcard._id}>
+                                <div onClick={() => this.deleteflashcard(flashcard._id)}>
+                                    <button>X</button>
+                                </div>
+                                <input
+                                    type="text"
+                                    name="question"
+                                    value={flashcard.question}
+                                    onChange={(event) => this.handleChange(event, flashcard._id)}
+                                />
+                                <textarea
+                                    name="answer"
+                                    value={flashcard.answer}
+                                    onChange={(event) => this.handleChange(event, flashcard._id)}
+                                />
+                                <div onClick = {() => this.updateFlashcard(flashcard._id)}>
+                                <button>Save Changes</button>
+                                </div>
+                            </div>
                         )
                     })}
-                </div>
+                </div> */}
             </div>
         )
     }
