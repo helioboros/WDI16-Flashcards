619532dafe702fda108a8734147560d7b94dbe1b
helioboros <felinewraith@gmail.com>
2018-06-29 08:51:34 -0400

    flashcard page

---
+++ client/src/components/FlashcardPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+//some kind of function to display a random flashcard from a temporary array made
+//if answer is right remove it from the array
+//if answer is wrong or if user skips then roll again
+
+class FlashcardPage extends Component {
+    render() {
+        return (
+            <div>hungus bungus</div>
+        )
+    }
+}
+
+export default FlashcardPage
