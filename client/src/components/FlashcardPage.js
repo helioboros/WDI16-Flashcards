9e3284d89689d7ef4ce8486b0fbdeb4227f16a55
helioboros <felinewraith@gmail.com>
2018-06-29 14:04:21 -0400

    component did mount flashcard page

--- client/src/components/FlashcardPage.js
+++ client/src/components/FlashcardPage.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react"
+import axios from 'axios'
 
 class FlashcardPage extends Component {
+    state = {
+        subject: {},
+        flashcards: []
+    }
+    componentDidMount() {
+        const subjectId = this.props.match.params.subjectId
+
+        axios.get(`/api/subjects/${subjectId}/flashcards`).then((res) => {
+            this.setState({
+                subject: res.data.subject,
+                flashcards: res.data.subject.flashcards
+            })
+        })
+    }
     //handleChange
     //updateFlashcard
     render() {
