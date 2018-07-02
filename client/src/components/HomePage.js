eee0a3283bdc77d526a70d50e2fe59da0c78c3d8
helioboros <felinewraith@gmail.com>
2018-07-02 08:54:37 -0400

    working on styled components

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Heading = styled.div`
+
+`
+const Caption = styled.div`
+
+`
 
 class HomePage extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <h1>Hello From HomePage</h1>
+        <Heading>
+          <h1>Welcome to the home page</h1>
+        </Heading>
+        <Caption>Why don't you click above for a list of subjects (:</Caption>
       </div>
     )
   }
--- client/src/components/NewSubjectForm.js
+++ client/src/components/NewSubjectForm.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const SubjectForm = styled.div`
+
+`
 
 class NewSubjectForm extends Component {
     state = {
@@ -25,17 +30,19 @@ class NewSubjectForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <label htmlFor="title">Title: </label>
-                    <input onChange={this.handleChange} type="text" name="title" value={this.state.title} />
-                </div>
-                <div>
-                    <label htmlFor="description">Description: </label>
-                    <input onChange={this.handleChange} type="text" name="description" value={this.state.description} />
-                </div>
-                <button>Submit</button>
-            </form>
+            <SubjectForm>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="title">Title: </label>
+                        <input onChange={this.handleChange} type="text" name="title" value={this.state.title} />
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description: </label>
+                        <input onChange={this.handleChange} type="text" name="description" value={this.state.description} />
+                    </div>
+                    <button>Submit</button>
+                </form>
+            </SubjectForm>
         )
     }
 }
